4e9ce57f4eec6c58f3d7eafb233bfc89eaaa69b7
Hasan Akgün <hasanakg@outlook.com>
2017-01-18 16:53:50 +0300

    Coklanan emojiler silindi
    Emojileri birden fazla defa eklemeye gerek yok diye dusunuyorum. 
    Coklanan emojilerin coklanmamasini saglayip, kisinin oy verdigi emojinin 
    yanindaki rakami yesile cevirerek ifadesini gosterebiliyor su an.

--- script.js
+++ script.js
@@ -8,7 +8,6 @@ var $authorLink = $topNavigation.find("a[href*='/biri/']");
 var titles =["Eğlenceli", "Harika tespit", "Bilgilendirici", "Ufkum 2x", "Troll", "Çöp"];
 AUTHOR = $authorLink.attr("title");
 
-
 // 0. check if session exists, if exists get sessionid
 chrome.runtime.sendMessage({cmd: "session"}, function(cookie) {
 	  if (cookie.id != null) {
@@ -82,7 +81,7 @@ $(document).on('mouseenter', '#entry-list li', function(e){
 	e.preventDefault();
 	var ep = $(this).find('.all-emotions'), entryid = ep.data("id"), ispop = ep.is(':empty');
 		emolist(ep, entryid).show();
-		highlight(entryid, ep);
+		highlight(entryid, $(this).find(".inner-emotions"));
 });
 
 
@@ -97,16 +96,11 @@ $(document).on('mouseleave', '#entry-list li', function(e){
 $(document).on('click', '.emotion-node', function(e){
 	e.preventDefault();
 	var _this = $(this), dd = $(this).attr("href");
-
-
-if (!$(_this).hasClass("emotion-stat-state")) {
 	chrome.runtime.sendMessage({cmd: "vote", dd: dd, author: AUTHOR, topic: $topic, topicid: $topicid, session : SESSIONID}, function(data) {	
-		console.log(data);
 			updateglobal(data);
-			redraw(_this.parent().parent().parent().parent().find(".inner-emotions"), dd.split('/')[2], [data]);			
+			redraw(_this.parent().parent().parent().parent().find(".inner-emotions"), dd.split('/')[2], [data]);
+			highlight(dd.split('/')[2], _this.parent().parent().parent().parent().find(".inner-emotions"));
     	});
-		$(_this).parent().hide();
-}
 });
 
 
@@ -114,7 +108,7 @@ function highlight(entryid, element) {
 	var currentdata = find(entryid, globaldata);
 	if (currentdata) {
 		var em = currentdata.emotionid;
-		element.find('.emotion-icon_' + em).parent().css("opacity", 1.0);
+		element.find('.emotion-icon_' + em).parent().parent().find('.emotion-stats').css("color", "#53a245");
 	}
 }
 
@@ -124,17 +118,17 @@ function redraw(innerdiv, entry, data) {
 var targetdiv = innerdiv.empty(), edata = find(entry, data)	
 			for (var property in edata) {
 				   
-			    if (edata.hasOwnProperty(property)) {		    	
-			        if (property.substring(0,2) == "e_" && property.split('_')[1] != "0" && edata[property] > 0)  {
-			        drawEmotion(targetdiv, property.split('_')[1], false, edata[property]);
+			    if (edata.hasOwnProperty(property)) {
+			        if (property.substring(0,2) == "e_" && property.split('_')[1] != "0" && edata[property] > 0)  {			        	
+			        	drawEmotion(targetdiv, property.split('_')[1], entry, edata[property]);
 			        }
 			    }
 			}
-
+		highlight(entry, targetdiv);
 }
 
 
-function drawEmotion(div, id, islink, value) {
+function drawEmotion(div, id, entryId, value) {
 	var span = $("<span/>").addClass("emotion-node").appendTo(div),
 		emocontainer = $("<div/>").addClass("emotion-container").appendTo(span),
 		emowrapper = $("<div/>").addClass("emotion-wrapper").appendTo(emocontainer),
@@ -144,20 +138,31 @@ function drawEmotion(div, id, islink, value) {
 	var emotitlewrapper = $("<div/>").addClass("emotion-title-wrapper").appendTo(emowrapper);
 		$("<div/>").addClass("emotion-title-text").html(titles[id - 1]).appendTo(emotitlewrapper);
 		
-		if (islink) {
-			span.attr("href", "vote/" + id + "/" + value);
-		} else {
-			span.addClass("emotion-stat-state");
-			emoicon.addClass("emotion-icon-style_stat");
-			$("<span/>").addClass("emotion-stats").html(value). appendTo(emowrapper);
-		}
+			span.attr("href", "vote/" + id + "/" + entryId);
+			if (value) {
+				span.addClass("emotion-stat-state");
+				emoicon.addClass("emotion-icon-style_stat");
+				$("<span/>").addClass("emotion-stats").html(value). appendTo(emowrapper);				
+			}
 }
 
-function emolist(div, entryid) {
+function emolist(div, entryid, data) {
 	div.empty();
+	var $entry = $('li[data-id=' + entryid + '] footer');
+	var $nodes = $entry.find('.inner-emotions .emotion-node.emotion-stat-state');
+	var emojis = [];
+	$nodes.each(function(index, element){
+		var $el = $(element);
+		var emojiId = $el.attr('href').split('/')[1];
+		emojis.push(parseInt(emojiId));
+	});
+
 	for (var i = 1; i <= titles.length; i++) {
-		drawEmotion(div, i, true, entryid);
+		if (emojis.indexOf(i) == -1) {
+			drawEmotion(div, i, entryid);
+		}
 	}
 	return div;
 }
+
 })(jQuery);
